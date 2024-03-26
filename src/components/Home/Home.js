ce743997509daa8727ed07628871b0b66fe2344a
aysbukre <aysbukre@gmail.com>
2024-03-26 16:09:04 +0300

    docker deployment last update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,7 +14,7 @@ function Home() {
       }
 
     const refreshPosts = () => {
-        fetch("/posts")
+        fetch("/api/posts")
             .then(res => res.json())
             .then(
                 (result) => {
